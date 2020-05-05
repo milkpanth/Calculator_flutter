e71cc55bb7ca17bc2b0869d4f7d27a3bd30cf7b1
milkpanth <milkpanth@yahoo.com>
2020-05-06 01:46:33 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'calculator',
+      home: Scaffold(
+        appBar: AppBar(title: Text("Calculator by  Nichachawalee Chatwattananon 6035512029")),
+        ),
+        
+    );
+  }
+}
+
+class Calculator extends StatefulWidget{
+  
+}
