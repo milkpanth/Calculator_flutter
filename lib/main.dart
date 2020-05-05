26047fa7ecfbb34b9f8e8652f7007f6728606340
milkpanth <milkpanth@yahoo.com>
2020-05-06 01:53:00 +0700

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'calculator',
-      home: Scaffold(
-        appBar: AppBar(title: Text("Calculator by  Nichachawalee Chatwattananon 6035512029")),
-        ),
-        
+      home: Calculator(),
+        // appBar: AppBar(title: Text("Calculator by  Nichachawalee Chatwattananon 6035512029")),
     );
   }
 }
 
 class Calculator extends StatefulWidget{
-  
+  CalculatorState createState() => CalculatorState();
+}
+
+class CalculatorState extends State<Calculator>{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Calculator by Nichachawalee Chatwattananon 6035512029"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text("Calculator"),
+        ],)
+      )
+    );
+  }
 }
